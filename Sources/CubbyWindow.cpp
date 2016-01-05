7867766bb36b3cd98473137a1b3a0f66eda8225e
Chris Ohk <utilForever@gmail.com>
2016-01-06 02:31:04 +0900

    Create CubbyWindow
    Cubby window class

---
+++ Sources/CubbyWindow.cpp
@@ -0,0 +1,73 @@
+/*************************************************************************
+> File Name: CubbyWindow.cpp
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: Cubby window class.
+> Created Time: 2016/01/06
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#include "Libraries/glew/include/GL/glew.h"
+
+#include <GL/gl.h>
+
+#include <stdlib.h>
+
+#pragma comment (lib, "opengl32")
+#pragma comment (lib, "glu32")
+
+#include "CubbyWindow.h"
+
+CubbyWindow::CubbyWindow()
+	: m_pWindow(nullptr),
+	m_windowWidth(500),
+	m_windowHeight(500)
+{
+	
+}
+
+CubbyWindow::~CubbyWindow()
+{
+	
+}
+
+void CubbyWindow::Create()
+{
+	// Initialize the window library
+	if (!glfwInit())
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	// Initialize any rendering params
+	int samples = 8;
+	glfwWindowHint(GLFW_SAMPLES, samples);
+	glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
+	glGetIntegerv(GL_SAMPLES_ARB, &samples);
+
+	// Create a windowed mode window and it's OpenGL context
+	m_pWindow = glfwCreateWindow(m_windowWidth, m_windowHeight, "Cubby", NULL, NULL);
+	if (!m_pWindow)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	/* Initialize this window object */
+	InitializeWindowContext(m_pWindow);
+}
+
+void CubbyWindow::InitializeWindowContext(GLFWwindow* window)
+{
+	// Center on screen
+	const GLFWvidmode* vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+	glfwGetWindowSize(window, &m_windowWidth, &m_windowHeight);
+	glfwSetWindowPos(window, (vidmode->width - m_windowWidth) / 2, (vidmode->height - m_windowHeight) / 2);
+
+	// Make the window's context current
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(true);
+
+	// Show the window
+	glfwShowWindow(window);
+}
---
+++ Sources/CubbyWindow.h
@@ -0,0 +1,30 @@
+/*************************************************************************
+> File Name: CubbyWindow.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: Cubby window class.
+> Created Time: 2016/01/06
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#pragma once
+
+#include <GLFW/glfw3.h>
+
+class CubbyWindow
+{
+public:
+	CubbyWindow();
+	~CubbyWindow();
+
+	void Create();
+	void InitializeWindowContext(GLFWwindow* window);
+
+private:
+	// The window object pointer
+	GLFWwindow* m_pWindow;
+
+	// Window dimensions
+	int m_windowWidth;
+	int m_windowHeight;
+};
