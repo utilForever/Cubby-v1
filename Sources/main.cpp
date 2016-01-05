9b30d4fc7d1f1c433bb7574fb96fb31e09c0946f
Chris Ohk <utilForever@gmail.com>
2016-01-06 02:30:45 +0900

    Add comments, Create window
    The main entry point for the application

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
+/*************************************************************************
+> File Name: main.cpp
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: The main entry point for the application.
+> Created Time: 2016/01/05
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#include "CubbyWindow.h"
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello, World!" << std::endl;
+	CubbyWindow cubbyWindow;
 
-	return 0;
+	cubbyWindow.Create();
+
+	while (true)
+	{
+
+	}
 }
