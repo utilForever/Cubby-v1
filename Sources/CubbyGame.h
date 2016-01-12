30649c1bb5f08e7be280c0c57fd26aee0db8297f
Chris Ohk <utilForever@gmail.com>
2016-01-13 03:16:49 +0900

    Add FPS and deltatime variables

--- Sources/CubbyGame.h
+++ Sources/CubbyGame.h
@@ -36,6 +36,18 @@ public:
 private:
 	CubbyWindow* m_pCubbyWindow;
 
+	// FPS and deltatime
+#ifdef _WIN32
+	LARGE_INTEGER m_fpsPreviousTicks;
+	LARGE_INTEGER m_fpsCurrentTicks;
+	LARGE_INTEGER m_fpsTicksPerSecond;
+#else
+	double m_fpsPreviousTicks;
+	double m_fpsCurrentTicks;
+#endif
+	float m_deltaTime;
+	float m_fps;
+
 	// Singleton instance
 	static CubbyGame* m_instance;
 };
