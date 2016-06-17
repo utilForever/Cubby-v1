620d7719d18c259e1b3dbec60c861580d74de6fe
Chris Ohk <utilForever@gmail.com>
2016-06-18 00:04:22 +0900

    Implement conversion functions

--- Sources/Maths/3DMaths.h
+++ Sources/Maths/3DMaths.h
@@ -1,1 +1,28 @@
-#pragma once
+/*************************************************************************
+> File Name: 3DMaths.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: 3D maths library.
+> Created Time: 2016/06/18
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_3D_MATH_H
+#define CUBBY_3D_MATH_H
+
+const float PI = 3.14159265358979323846f;
+
+// Conversions
+template <typename T>
+T DegreeToRadian(const T& degrees)
+{
+	return (degrees * PI) / 180;
+}
+
+template <typename T>
+T RadianToDegree(const T& radians)
+{
+	return (radians * 180) / PI;
+}
+
+#endif
