4c873b9238e1df5e84305ebfbe79af596a5c8dae
Chris Ohk <utilForever@gmail.com>
2016-06-18 15:41:59 +0900

    Add Line3D.h

---
+++ Sources/Maths/Line3D.h
@@ -0,0 +1,37 @@
+/*************************************************************************
+> File Name: Line3D.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: 3d maths - Line 3D library.
+> Created Time: 2016/06/18
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_LINE_3D_H
+#define CUBBY_LINE_3D_H
+
+#include "../Libraries/glm/vec3.hpp"
+
+class Line3D
+{
+public:
+	// Constructors
+	Line3D();
+	Line3D(glm::vec3 lStart, glm::vec3 lEnd);
+	Line3D(float x1, float y1, float z1, float x2, float y2, float z2);
+
+	// Properties
+	const glm::vec3 GetMidPoint() const;
+	const glm::vec3 GetVector() const;
+	const float GetLength() const;
+	const float GetLengthSquared() const;
+
+	// Operations
+	const glm::vec3 GetInterpolatedPoint(float t) const;
+
+private:
+	glm::vec3 m_startPoint;
+	glm::vec3 m_endPoint;
+};
+
+#endif
