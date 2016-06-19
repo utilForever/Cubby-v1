b7c87fcaa5b197de9e49877a193d8695ef0f4dfb
Chris Ohk <utilForever@gmail.com>
2016-06-20 00:47:09 +0900

    Add Bezier4.h

---
+++ Sources/Maths/Bezier4.h
@@ -0,0 +1,33 @@
+/*************************************************************************
+> File Name: Bezier4.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: Bezier Curve with 4 points - 2 control points.
+> Created Time: 2016/06/19
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_BEZIER4_H
+#define CUBBY_BEZIER4_H
+
+#include "../Libraries/glm/vec3.hpp"
+
+class Bezier4
+{
+public:
+	// Constructors
+	Bezier4();
+	Bezier4(glm::vec3 start, glm::vec3 end, glm::vec3 control1, glm::vec3 control2);
+	Bezier4(float xStart, float yStart, float zStart, float xEnd, float yEnd, float zEnd, float xControl1, float yControl1, float zControl1, float xControl2, float yControl2, float zControl2);
+
+	// Operations
+	const glm::vec3 GetInterpolatedPoint(float t) const;
+
+private:
+	glm::vec3 m_startPoint;
+	glm::vec3 m_endPoint;
+	glm::vec3 m_controlPoint1;
+	glm::vec3 m_controlPoint2;
+};
+
+#endif
