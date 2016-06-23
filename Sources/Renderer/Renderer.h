e0f0988afe28a3e7abfd88f8ec344e21b0f2210c
Chris Ohk <utilForever@gmail.com>
2016-06-24 00:47:02 +0900

    Add Renderer.h

---
+++ Sources/Renderer/Renderer.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+> File Name: Renderer.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: The OpenGL renderer that is an encapsulation of all the rendering functionality of the engine. A wrapper around most common OpenGL calls.
+> Created Time: 2016/06/24
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_RENDERER_H
+#define CUBBY_RENDERER_H
+
+#endif
