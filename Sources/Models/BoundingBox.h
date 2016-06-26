1325105226206e5500d6f2874ea8ac46e39e05c8
Chris Ohk <utilForever@gmail.com>
2016-06-26 23:05:22 +0900

    Add BoundingBox.h

---
+++ Sources/Models/BoundingBox.h
@@ -0,0 +1,47 @@
+/*************************************************************************
+> File Name: BoundingBox.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: Bounding box.
+> Created Time: 2016/06/26
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_BOUNDING_BOX_H
+#define CUBBY_BOUNDING_BOX_H
+
+#include <glm/vec3.hpp>
+
+class BoundingBox
+{
+public:
+	// Constructor, Destructor
+	BoundingBox();
+	~BoundingBox();
+
+	// Getter
+	float GetMinX() const;
+	float GetMinY() const;
+	float GetMinZ() const;
+
+	float GetMaxX() const;
+	float GetMaxY() const;
+	float GetMaxZ() const;
+
+	float GetWidth() const;
+	float GetHeight() const;
+	float GetLength() const;
+
+	glm::vec3 GetCenter() const;
+
+private:
+	float m_minX;
+	float m_minY;
+	float m_minZ;
+
+	float m_maxX;
+	float m_maxY;
+	float m_maxZ;
+};
+
+#endif
