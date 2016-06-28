5dacf64a8da75a278e3cfd27dc2298ee4bd2f0a2
Chris Ohk <utilForever@gmail.com>
2016-06-28 12:26:08 +0900

    Add MS3DAnimator.h

---
+++ Sources/Models/MS3DAnimator.h
@@ -0,0 +1,164 @@
+/*************************************************************************
+> File Name: MS3DAnimator.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: Milk Shape 3D Animator.
+> Created Time: 2016/06/28
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_MS3D_ANIMATOR_H
+#define CUBBY_MS3D_ANIMATOR_H
+
+#include "MS3DModel.h"
+#include "Renderer/Renderer.h"
+
+// Joint animation structure
+typedef struct JointAnimation
+{
+	int currentTranslationKeyframe;
+	int currentRotationKeyframe;
+
+	float startBlendTrans[3];
+	float endBlendTrans[3];
+	float startBlendRot[3];
+	float endBlendRot[3];
+	float currentBlendTrans[3];
+	float currentBlendRot[3];
+
+	Matrix4 finalMatrix;
+
+} JointAnimation;
+
+// Animation structure
+static const int MAX_ANIMATION_NAME = 64;
+
+typedef struct Animation
+{
+	int startFrame;
+	int endFrame;
+	int blendFrame;
+	double startTime;
+	double endTime;
+	bool looping;
+	int startRightWeaponTrailFrame;
+	int endRightWeaponTrailFrame;
+	double startRightWeaponTrailTime;
+	double endRightWeaponTrailTime;
+	int startLeftWeaponTrailFrame;
+	int endLeftWeaponTrailFrame;
+	double startLeftWeaponTrailTime;
+	double endLeftWeaponTrailTime;
+	char animationName[MAX_ANIMATION_NAME];
+} Animation;
+
+class MS3DAnimator
+{
+public:
+	// Constructor, Destructor
+	MS3DAnimator(Renderer* pRenderer, MS3DModel* pModel);
+	~MS3DAnimator();
+
+	// Animation
+	MS3DModel* GetModel();
+
+	void CreateJointAnimations();
+
+	bool LoadAnimations(const char* animationFileName);
+
+	void CalculateBoundingBox();
+	BoundingBox* GetBoundingBox();
+
+	void PlayAnimation(int animationIndex);
+	void PlayAnimation(const char* animationName);
+	void PauseAnimation();
+	void ResumeAnimation();
+
+	int GetCurrentAnimationIndex() const;
+	int GetNumAnimations() const;
+	const char* GetAnimationName(const int index);
+	bool IsAnimationPaused() const;
+	bool HasAnimationLooped() const;
+	bool HasAnimationFinished() const;
+
+	int GetStartFrame(const char* animationName);
+	int GetEndFrame(const char* animationName);
+	int GetCurrentFrame();
+
+	float GetAnimationTimer();
+	int GetNumKeyframes();
+
+	// Blending
+	void StartBlendAnimation(int startIndex, int endIndex, float blendTime);
+	void StartBlendAnimation(const char* startAnimationName, const char* endAnimationName, float blendTime);
+	void BlendIntoAnimation(const char* animationName, float blendTime);
+
+	void GetCurrentBlendTranslation(int jointIndex, float* x, float* y, float* z);
+	void GetCurrentBlendRotation(int jointIndex, float* x, float* y, float* z);
+
+	bool GetRightWeaponTrailActive();
+	bool GetLeftWeaponTrailActive();
+
+	void Restart();
+
+	void SetTimerForStartOfAnimation();
+
+	Matrix4 GetBoneMatrix(int index);
+
+	// Update
+	void Update(float dt);
+	void UpdateBlending(float dt);
+
+	// Rendering
+	void Render(bool isMesh, bool isNormal, bool isBone, bool isBoundingBox);
+	void RenderMesh();
+	void RenderNormals();
+	void RenderBones();
+	void RenderBoundingBox();
+
+private:
+	Renderer* m_pRenderer;
+
+	// The MS3D model for this animator
+	MS3DModel* m_pModel;
+
+	// Joint animations
+	int m_numJointAnimations;
+	JointAnimation* m_pJointAnimations;
+
+	// Animations
+	int m_numAnimations;
+	Animation* m_pAnimations;
+
+	// Current playing animation
+	int m_currentAnimationIndex;
+	double m_currentAnimationStartTime;
+	double m_currentAnimationEndTime;
+
+	// Current animation timers for the weapon trail start and stop times
+	double m_currentAnimationRightWeaponTrailStartTime;
+	double m_currentAnimationRightWeaponTrailEndTime;
+	double m_currentAnimationLeftWeaponTrailStartTime;
+	double m_currentAnimationLeftWeaponTrailEndTime;
+
+	// Current animation timer and local looping variable
+	double m_timer;
+	bool m_isLooping;
+
+	// Status flags
+	bool m_bPaused;
+	bool m_bLooped;
+	bool m_bFinished;
+
+	// Blending
+	bool m_isBlending;
+	float m_blendTime;
+	double m_blendTimer;
+	int m_blendStartAnimationIndex;
+	int m_blendEndAnimationIndex;
+
+	// Bounding box
+	BoundingBox m_BoundingBox;
+};
+
+#endif
