31679e272a848e712726a2293c4d1faa5158e2eb
Chris Ohk <utilForever@gmail.com>
2016-07-06 12:41:35 +0900

    Add some behavior in main loop, code refactoring

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -2,35 +2,48 @@
 > File Name: main.cpp
 > Project Name: Cubby
 > Author: Chan-Ho Chris Ohk
-> Purpose: The main entry point for the application.
-> Created Time: 2016/01/05
+> Purpose
+>    The main entry point for the application. Creates the initial instances of
+>    the container classes, iterates the game loop, polls events/input and
+>    detects for game closure to cleanup the with the destruction.
+> Created Time: 2016/07/06
 > Copyright (c) 2016, Chan-Ho Chris Ohk
 *************************************************************************/
 
 #include "CubbyGame.h"
 
-int main(int argc, char* argv[])
+int main()
 {
+	CubbySettings* pCubbySettings = new CubbySettings();
+	pCubbySettings->LoadSettings();
+	pCubbySettings->LoadOptions();
+
 	// Initialize and create the CubbyGame object
 	CubbyGame* pCubbyGame = CubbyGame::GetInstance();
-	pCubbyGame->Create();
+	pCubbyGame->Create(pCubbySettings);
 
 	// Loop until the user closes the window
 	while (!pCubbyGame->ShouldClose())
 	{
 		// Poll input events
 		pCubbyGame->PollEvents();
+
+		// Update joysticks
+		pVoxGame->UpdateJoySticks();
 		
 		// Update
 		pCubbyGame->Update();
 
 		// PreRender
-		// pCubbyGame->PreRender();
+		pCubbyGame->PreRender();
 
 		// Render
-		// pCubbyGame->Render();
+		pCubbyGame->Render();
 	}
 
 	// Cleanup
-	// pCubbyGame->Destroy();
+	pCubbyGame->Destroy();
+
+	// Exit
+	exit(EXIT_SUCCESS);
 }
