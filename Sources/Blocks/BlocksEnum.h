d0ac003d1b24734a04851a3f4c84db4fb72ff185
Chris Ohk <utilForever@gmail.com>
2016-07-08 13:07:14 +0900

    Add BlocksEnum.h

---
+++ Sources/Blocks/BlocksEnum.h
@@ -0,0 +1,30 @@
+/*************************************************************************
+> File Name: BlocksEnum.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: An enum list of all of the block types.
+> Created Time: 2016/07/08
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_BLOCKS_ENUM_H
+#define CUBBY_BLOCKS_ENUM_H
+
+enum class BlockType
+{
+	Default = 0,
+	UndergroundIndestructable,
+	Stone,
+	Dirt,
+	Grass,
+	Rock,
+	Snow,
+	Wood,
+	Sand,
+	Cactus,
+	Leaf,
+	CustomColour,
+	NumTypes,
+};
+
+#endif
