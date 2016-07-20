871fc415465965ac406e0ac1c7bf734e39b8622e
Chris Ohk <utilForever@gmail.com>
2016-07-21 02:21:25 +0900

    Add EquipmentEnum.h

---
+++ Sources/Items/EquipmentEnum.h
@@ -0,0 +1,121 @@
+/*************************************************************************
+> File Name: EquipmentEnum.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose
+>    An enum list of all of the equipment in the game, used for equipment
+>    tracking and equipment specific functionality.
+> Created Time: 2016/07/21
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_EQUIPMENT_ENUM_H
+#define CUBBY_EQUIPMENT_ENUM_H
+
+enum class Equipment
+{
+	None = 0,
+
+	// Weapons
+	NormalPickaxe,
+	Torch,
+	Hammer,
+	MageStaff,
+	NecroStaff,
+	PriestStaff,
+	DruidStaff,
+	TwoHandedSword,
+	Boomerang,
+	Bomb,
+	KnifeLeft,
+	KnifeRight,
+	FireballHandLeft,
+	FireballHandRight,
+	Mace,
+	Sickle,
+	DragonBow,
+
+	// Equipment
+	BlacksmithGloves,
+	NormalBoots,
+	NormalGloves,
+	NormalShoulders,
+	RocketBoots,
+	RegrowthBoots,
+
+	// Wooden
+	WoodenSword,
+	WoodenShield,
+	WoodenBow,
+	WoodenHelm,
+	WoodenArmor,
+	WoodenPants,
+	WoodenGloves,
+	WoodenBoots,
+	WoodenShoulders,
+
+	// Cooper
+	// TODO: Add equipments
+
+	// Iron
+	IronSword,
+	IronShoeld,
+	IronHelm,
+	IronArmor,
+	IronPants,
+	IronGloves,
+	IronBoots,
+	IronShoulders,
+
+	// Steel
+	SteelHelm,
+	SteelArmor,
+	SteelPants,
+	SteelGloves,
+	SteelBoots,
+	SteelShoulders,
+
+	// Silver
+	// TODO: Add equipments
+
+	// Gold
+	// TODO: Add equipments
+
+	// Ash
+	AshSword,
+	AshHelm,
+	AshArmor,
+	AshPants,
+	AshGloves,
+	AshBoots,
+	AshShoulders,
+
+	// Bone
+	BoneSword,
+	BoneHelm,
+	BoneArmor,
+	BonePants,
+	BoneGloves,
+	BoneBoots,
+	BoneShoulders,
+
+	// Ice
+	IceHelm,
+	IceArmor,
+	IcePants,
+	IceGloves,
+	IceBoots,
+	IceShoulders,
+
+	// Spike
+	SpikeHelm,
+	SpikeArmor,
+	SpikePants,
+	SpikeGloves,
+	SpikeBoots,
+	SpikeShoulders,
+
+	NUM_EQUIPMENT,
+};
+
+#endif
