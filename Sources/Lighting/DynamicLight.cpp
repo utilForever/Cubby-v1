e510294b7c2c79abba1528b633c9eb18301d319e
Chris Ohk <utilForever@gmail.com>
2016-07-25 03:24:39 +0900

    Implement getters / setters

--- Sources/Lighting/DynamicLight.cpp
+++ Sources/Lighting/DynamicLight.cpp
@@ -22,6 +22,87 @@ DynamicLight::~DynamicLight()
 
 }
 
+// Getter, Setter
+glm::vec3 DynamicLight::GetPosition() const
+{
+	return m_position;
+}
+
+float DynamicLight::GetRadius() const
+{
+	return m_radius;
+}
+
+float DynamicLight::GetDiffuseScale() const
+{
+	return m_diffuseScale;
+}
+
+Color DynamicLight::GetColor() const
+{
+	return m_color;
+}
+
+unsigned int DynamicLight::GetLightID() const
+{
+	return m_lightID;
+}
+
+float DynamicLight::GetLifeTime() const
+{
+	return m_lifeTime;
+}
+
+float DynamicLight::GetMaxLifeTime() const
+{
+	return m_maxLifeTime;
+}
+
+bool DynamicLight::IsDyingLight() const
+{
+	return m_isDyingLight;
+}
+
+void DynamicLight::SetPosition(glm::vec3 position)
+{
+	m_position = position;
+}
+
+void DynamicLight::SetRadius(float radius)
+{
+	m_radius = radius;
+}
+
+void DynamicLight::SetDiffuseScale(float diffuseScale)
+{
+	m_diffuseScale = diffuseScale;
+}
+
+void DynamicLight::SetColor(Color color)
+{
+	m_color = color;
+}
+
+void DynamicLight::SetLightID(unsigned int lightID)
+{
+	m_lightID = lightID;
+}
+
+void DynamicLight::SetLifeTime(float lifeTime)
+{
+	m_lifeTime = lifeTime;
+}
+
+void DynamicLight::SetMaxLifeTime(float maxLifeTime)
+{
+	m_maxLifeTime = maxLifeTime;
+}
+
+void DynamicLight::SetDyingLight(bool isDyingLight)
+{
+	m_isDyingLight = isDyingLight;
+}
+
 // Erase operations
 void DynamicLight::SetErase(bool erase)
 {
