daec5053174baeb12e56e96ed07db6f414171fc7
Chris Ohk <utilForever@gmail.com>
2016-07-30 18:31:23 +0900

    Change class to struct, and implement it

--- Sources/Inventory/InventoryManager.h
+++ Sources/Inventory/InventoryManager.h
@@ -17,6 +17,11 @@
 
 #include <vector>
 
+#include "ItemsEnum.h"
+#include "StatAttribute.h"
+
+using StatAttributeList = std::vector<StatAttribute*>;
+
 // Item types
 enum class InventoryType
 {
@@ -88,9 +93,113 @@ enum class ItemStatus
 	Exploding,
 };
 
-class InventoryItem
+struct InventoryItem
 {
+	// Destructor
+	~InventoryItem()
+	{
+		ClearAttributes();
+	}
 
+	void ClearAttributes()
+	{
+		for (size_t i = 0; i < m_vpStatAttributes.size(); ++i)
+		{
+			delete m_vpStatAttributes[i];
+			m_vpStatAttributes[i] = nullptr;
+		}
+
+		m_vpStatAttributes.clear();
+	}
+
+	void AddStatAttribute(AttributeType type, int amount)
+	{
+		StatAttribute* pAttribute = new StatAttribute(type, amount);
+
+		m_vpStatAttributes.push_back(pAttribute);
+	}
+
+	std::string GetStatsAttributeString()
+	{
+		std::string returnString = "";
+
+		if (m_vpStatAttributes.size() > 0)
+		{
+			returnString += "\n";
+		}
+
+		for (size_t i = 0; i < m_vpStatAttributes.size(); ++i)
+		{
+			std::string statAttribute = m_vpStatAttributes[i]->GetString();
+
+			returnString += statAttribute;
+
+			if (i < m_vpStatAttributes.size() - 1)
+			{
+				returnString += "\n";
+			}
+		}
+
+		return returnString;
+	}
+
+	// Filename
+	std::string m_fileName;
+
+	// Icon filename for GUI
+	std::string m_IconfileName;
+
+	// Title
+	std::string m_title;
+
+	// Description
+	std::string m_description;
+
+	// Item type
+	InventoryType m_itemType;
+
+	// Item
+	ItemType m_item;
+
+	// Status effects
+	ItemStatus m_status;
+
+	// Equip slot
+	EquipSlot m_equipSlot;
+
+	// Quality
+	ItemQuality m_itemQuality;
+
+	// Display left or right sprites
+	bool m_left;
+	bool m_right;
+
+	// Placement block colour
+	float m_placementR;
+	float m_placementG;
+	float m_placementB;
+
+	// Loot GUI placement
+	int m_lootSlotX;
+	int m_lootSlotY;
+
+	// Is the item equipped or not
+	bool m_equipped;
+
+	// Scale and offset
+	float m_scale;
+	float m_offsetX;
+	float m_offsetY;
+	float m_offsetZ;
+
+	// Quantity
+	int m_quantity;
+
+	// Stat modifiers
+	StatAttributeList m_vpStatAttributes;
+
+	// Erase flag
+	bool m_remove;
 };
 
 using InventoryItemList = std::vector<InventoryItem*>;
