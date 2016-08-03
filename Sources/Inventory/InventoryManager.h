62a84e5875934cfa46c5405c8590964af459e9d9
Chris Ohk <utilForever@gmail.com>
2016-08-03 13:04:05 +0900

    Add struct, methods, Edit include files

--- Sources/Inventory/InventoryManager.h
+++ Sources/Inventory/InventoryManager.h
@@ -17,8 +17,9 @@
 
 #include <vector>
 
-#include "ItemsEnum.h"
-#include "StatAttribute.h"
+#include <Items/EquipmentEnum.h>
+#include <Items/ItemsEnum.h>
+#include <Items/StatAttribute.h>
 
 using StatAttributeList = std::vector<StatAttribute*>;
 
@@ -91,6 +92,14 @@ enum class ItemStatus
 	Ice,
 	Poison,
 	Exploding,
+};
+
+struct ItemTextData
+{
+	ItemType m_item;
+	std::string m_fileName;
+	std::string m_title;
+	std::string m_description;
 };
 
 struct InventoryItem
@@ -174,7 +183,7 @@ struct InventoryItem
 	bool m_left;
 	bool m_right;
 
-	// Placement block colour
+	// Placement block color
 	float m_placementR;
 	float m_placementG;
 	float m_placementB;
@@ -206,7 +215,128 @@ using InventoryItemList = std::vector<InventoryItem*>;
 
 class InventoryManager
 {
+public:
+	// Constructor, Destructor
+	InventoryManager();
+	~InventoryManager();
 
+	// Linkage
+	void SetPlayer(Player* pPlayer);
+	void SetInventoryGUI(InventoryGUI* pInventoryGUI);
+	void SetLootGUI(LootGUI* pLootGUI);
+	void SetActionBar(ActionBar* pActionBar);
+
+	void Reset();
+
+	void ClearInventory();
+	void ClearEquipped();
+	void ClearOtherCreatedItems();
+	void LoadInventory(std::string playerName, PlayerClass playerClass, bool exportInventoryFile);
+	void LoadDefaultInventory();
+	void LoadInventoryForClass(PlayerClass playerClass);
+
+	void SetInventoryGUINeedsUpdate(bool update);
+	bool InventoryGUINeedsUpdate() const;
+
+	void SetCharacterGUINeedsUpdate(bool update);
+	bool CharacterGUINeedsUpdate() const;
+
+	// Supress export
+	void SetSupressExport(bool supress);
+
+	// Import / Export inventory
+	void ExportInventory(std::string playerName);
+	void ImportInventory(std::string playerName);
+
+	bool IsInventoryFull();
+
+	ItemTextData* GetItemTextData(ItemType item);
+	bool CanDestroyItemWithHammer(ItemType item);
+
+	bool CanAddInventoryItem(const char* title, ItemType item, int quantity);
+
+	InventoryItem* CreateInventoryItem(InventoryItem* pItem);
+	InventoryItem* CreateInventoryItem(const char* fileName, const char* iconFileName, InventoryType itemType, ItemType item, ItemStatus status, EquipSlot equipSlot, ItemQuality itemQuality, bool left, bool right, const char* title, const char* description, float r, float g, float b, int quantity, int lootSlotX, int lootSlotY, int setInventorySlotX, int setInventorySlotY);
+	InventoryItem* CreateInventoryItemForCrafting(ItemType item, int quantity, ItemQuality itemQuality);
+	InventoryItem* CreateEquipmentItemFromType(EquipmentType equipment);
+
+	InventoryItem* AddInventoryItem(const char* fileName, const char* iconFileName, InventoryType itemType, ItemType item, ItemStatus status, EquipSlot equipSlot, ItemQuality itemQuality, bool left, bool right, const char* title, const char* description, float r, float g, float b, int quantity, int lootSlotX, int lootSlotY, int setInventorySlotX, int setInventorySlotY);
+	InventoryItem* AddInventoryItem(InventoryItem* pInventoryItem, int inventoryX, int inventoryY);
+
+	void RemoveInventoryItem(const char* title, ItemType item, int quantity);
+	void RemoveInventoryItem(EquipSlot equipSlot);
+	void RemoveInventoryItem(int slotIndex);
+	void RemoveInventoryItem(int xPos, int yPos);
+
+	InventoryItem* GetInventoryItemForSlot(int slotIndex);
+	InventoryItem* GetInventoryItemForSlot(int xPos, int yPos);
+	InventoryItem* GetInventoryItemWithTitle(std::string title);
+
+	void SwitchInventoryItems(int slot1, int slot2);
+	void SwitchInventoryItems(int x1, int y1, int x2, int y2);
+
+	void SwitchEquippedSlots(EquipSlot equipSlotSrc, EquipSlot equipSlotDst);
+
+	void EquipInventoryItem(int slotIndex, EquipSlot equipSlot);
+	void EquipInventoryItem(int xPos, int yPos, EquipSlot equipSlot);
+
+	void EquipLootItem(int xPos, int yPos, InventoryItem* pInventoryItem, EquipSlot equipSlot);
+
+	void UnequipItem(int xPos, int yPos, EquipSlot equipSlot);
+	void UnequipItemToLootGUI(EquipSlot equipSlot);
+	bool UnequipItemToFreeInventorySlot(EquipSlot equipSlot, int* newSlotX, int* newSlotY);
+
+	InventoryItem* GetInventoryItemForEquipSlot(EquipSlot equipSlot);
+
+	bool IsEquippedStatus(ItemStatus status);
+
+	// Coins
+	int GetNumCoins();
+	void GiveCoins(int numCoins);
+	void TakeCoins(int numCoins);
+	bool CoinsUpdated();
+	void SetCoinsUpdated(bool set);
+
+	void Update(float dt);
+
+	// Constants
+	static const int MAX_NUM_SLOTS_HORIZONTAL = 6;
+	static const int MAX_NUM_SLOTS_VERTICAL = 3;
+	static const int MAX_NUM_INVENTORY_SLOTS = MAX_NUM_SLOTS_HORIZONTAL * MAX_NUM_SLOTS_VERTICAL;
+
+protected:
+	int ConvertSlotsToIndex(int x, int y);
+
+private:
+	bool m_InventoryGUINeedsUpdate;
+	bool m_CharacterGUINeedsUpdate;
+
+	// Player name
+	std::string m_playerName;
+
+	// List of inventory items in the GUI and equipped
+	InventoryItemList m_vpInventoryItemList;
+
+	// Slot mapping for inventory
+	InventoryItem* m_ItemSlotMapping[MAX_NUM_INVENTORY_SLOTS];
+
+	// SLot mapping for equipped items
+	InventoryItem* m_equippedSlots[static_cast<int>(EquipSlot::NumSlots)];
+
+	// List for deletion purposes, since inventory items get created for random loot and crafting and are stored in this manager.
+	InventoryItemList m_vpOtherInventoryItemList;
+
+	// Coins
+	int m_numCoins;
+	bool m_coinsUpdated;
+
+	// Force to stop export inventory file
+	bool m_supressExport;
+
+	Player* m_pPlayer;
+	InventoryGUI* m_pInventoryGUI;
+	LootGUI* m_pLootGUI;
+	ActionBar* m_pActionBar;
 };
 
 #endif
