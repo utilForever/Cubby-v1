68c23ce5f7e4e522f7ec18ba2f2a954a8d08fb07
Chris Ohk <utilForever@gmail.com>
2016-08-04 01:21:54 +0900

    Add PlayerClass.h

---
+++ Sources/Player/PlayerClass.h
@@ -0,0 +1,24 @@
+/*************************************************************************
+> File Name: PlayerClass.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: An enum list of all of the player classes.
+> Created Time: 2016/08/04
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_PLAYER_CLASS_H
+#define CUBBY_PLAYER_CLASS_H
+
+enum class PlayerClass
+{
+	Debug = 0,
+	Warrior,
+	Ranger,
+	Mage,
+	Priest,
+	Necromaner,
+	Knight,
+};
+
+#endif
