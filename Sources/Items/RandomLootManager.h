873b602bb7815196f5d5be1d12e7dc8712198bec
Chris Ohk <utilForever@gmail.com>
2016-08-03 13:03:28 +0900

    Add RandomLootManager.h

---
+++ Sources/Items/RandomLootManager.h
@@ -0,0 +1,60 @@
+/*************************************************************************
+> File Name: RandomLootManager.h
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: Random root manager class.
+> Created Time: 2016/08/03
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#ifndef CUBBY_RANDOM_LOOT_MANAGER_H
+#define CUBBY_RANDOM_LOOT_MANAGER_H
+
+#include "Item.h"
+
+struct EnemyIngredientsSpawnData
+{
+	EnemyType m_sourceEnemy;
+	ItemType m_spawnedItem;
+	int m_minSpawn;
+	int m_maxSpawn;
+};
+
+struct RandomLootItem
+{
+	InventoryItem* m_pLootItem;
+	EquipmentType m_equipmentType;
+	int m_repickValue;
+};
+
+using EnemyIngredientsSpawnDataList = std::vector<EnemyIngredientsSpawnData*>;
+using RandomLootItemList = std::vector<RandomLootItem*>;
+
+class RandomLootManager
+{
+public:
+	// Constructor, Destructor
+	RandomLootManager();
+	~RandomLootManager();
+
+	// Deletion
+	void ClearEnemyIngredientsSpawnData();
+	void ClearRandomLootList();
+
+	// Enemy dropping ingredients
+	void AddEnemyIngredientSpawnData(EnemyType sourceEnemy, ItemType spawnedItem, int minSpawn, int maxSpawn);
+	void GetSpawnedIngredientItemForEnemy(EnemyType sourceEnemy, ItemType* item, int* quantity);
+
+	// Random loot
+	void AddRandomLoot(EquipmentType equipment);
+	InventoryItem* GetRandomLootItem(EquipmentType* equipment);
+
+private:
+	// Enemy ingredients
+	EnemyIngredientsSpawnDataList m_vpEnemyIngredientsSpawnDataList;
+
+	// Random loot
+	RandomLootItemList m_vpRandomLootItemList;
+};
+
+#endif
