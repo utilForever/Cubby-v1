50b168c24ecb0d02bd0e58a64458b4df0b44fa1b
Chris Ohk <utilForever@gmail.com>
2016-08-25 02:25:34 +0900

    Add ProgressBar

---
+++ Sources/GUI/ProgressBar.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+> File Name: ProgressBar.cpp
+> Project Name: Cubby
+> Author: Chan-Ho Chris Ohk
+> Purpose: A pulldown menu GUI component.
+> Created Time: 2016/08/25
+> Copyright (c) 2016, Chan-Ho Chris Ohk
+*************************************************************************/
+
+#include "ProgressBar.h"
+
+// Constructor, Destructor
