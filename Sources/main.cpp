5b31b7571ee476a293ad8d2b2dba0b4c77432c71
Chris Ohk <utilForever@gmail.com>
2016-10-16 19:50:15 +0900

    Fix Issue #16: Support NVIDIA Optimus and AMD Enduro chipset

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -12,6 +12,14 @@
 
 #include "CubbyGame.h"
 
+#if defined(WIN32) || defined(_WIN32)
+extern "C" { __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001; }
+extern "C" { __declspec(dllexport) DWORD AmdPowerXpressRequestHighPerformance = 0x00000001; }
+#else 
+extern "C" { int NvOptimusEnablement = 1; }
+extern "C" { int AmdPowerXpressRequestHighPerformance = 1; }
+#endif
+
 int main()
 {
 	// Load the settings
