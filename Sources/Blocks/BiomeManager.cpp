3b7ca04ec7b9cc912b34b79e8404b16716e32c2b
Chris Ohk <utilforever@gmail.com>
2017-09-17 23:55:24 +0900

    Update BiomeManager.cpp - Fix crash problems due to deallocate nullptr in BiomeManager::GetBiome() (#40)

--- Sources/Blocks/BiomeManager.cpp
+++ Sources/Blocks/BiomeManager.cpp
@@ -155,26 +155,9 @@ void BiomeManager::AddSafeZone(glm::vec3 safeZoneCenter, float length, float hei
 // Get biome
 Biome BiomeManager::GetBiome(glm::vec3 position)
 {
-    float      regionValue;
-
-    RegionKey  regionKey;
-    RegionData regionData;
-
-    regionData.x = static_cast<short>(static_cast<int>(position.x) / Chunk::CHUNK_SIZE);
-    regionData.y = static_cast<short>(static_cast<int>(position.y) / Chunk::CHUNK_SIZE);
-    regionData.z = static_cast<short>(static_cast<int>(position.z) / Chunk::CHUNK_SIZE);
-
-    memcpy(&regionKey, &regionData, sizeof(RegionKey));
-
-    auto iterator = m_biomeCached.find(regionKey);
-
-    if (iterator == m_biomeCached.end())
-    {
-        regionValue = static_cast<float>(m_biomeRegions.GetValue(position.x, position.y, position.z));
-        m_biomeCached.insert(std::pair<RegionKey, float>(regionKey, regionValue));
-    } else regionValue = iterator->second;
-
-	float ratio = 1.0f / (static_cast<int>(Biome::NumBiomes) - 1.0f);
+	float regionValue = static_cast<float>(m_biomeRegions.GetValue(position.x, position.y, position.z));
+	
+   	float ratio = 1.0f / (static_cast<int>(Biome::NumBiomes) - 1.0f);
 	for (int i = 1; i < static_cast<int>(Biome::NumBiomes); ++i)
 	{
 		float maxValue = i * ratio;
